aba3c690043c03fb9052f205d4384f23a34f0d7e
Ilia <Ninetailedkot92@gmail.com>
2021-05-17 11:13:30 +0300

    First commit

---
+++ Assets/Scripts/HeroInputReader.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroInputReader : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+
+        }
+        else if (Input.GetKey(KeyCode.D))
+        { 
+        
+        }
+    }
+}
