ed3d716cea42ddb6d0fddd27d12466d1b9812e39
Ilia <Ninetailedkot92@gmail.com>
2021-05-17 11:41:02 +0300

    Add moveability

--- Assets/Scripts/HeroInputReader.cs
+++ Assets/Scripts/HeroInputReader.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class HeroInputReader : MonoBehaviour
 {
+    private Hero _hero;
+
+    private void Awake()
+    {
+        _hero = GetComponent<Hero>();            
+    }
+
     void Update()
     {
         if (Input.GetKey(KeyCode.A))
         {
-
+            _hero.SetDirection(-1);
         }
         else if (Input.GetKey(KeyCode.D))
-        { 
-        
+        {
+            _hero.SetDirection(1);
+        }
+        else 
+        {
+            _hero.SetDirection(0);
         }
     }
 }
