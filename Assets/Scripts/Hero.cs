3c91c08c275df8ad5f3fd0ff21eb14b6ef23ea24
Ilia <Ninetailedkot92@gmail.com>
2021-05-19 18:49:19 +0300

    Colliders
    Переделал коллайдеры у пальм.
    Настроил скорость прыжков/скорость
    движения/гравитацию

--- Assets/Scripts/Hero.cs
+++ Assets/Scripts/Hero.cs
@@ -38,11 +38,15 @@ public class Hero : MonoBehaviour
 
         var isJumping = _direction.y > 0;
 
-        //if (Mathf.Abs(_rigidbody.velocity.y) > 1f) isJumping = false;
 
-        if (isJumping && IsGrounded()) 
+        if (isJumping)
         {
-            _rigidbody.AddForce(Vector2.up * _jumpSpeed, ForceMode2D.Impulse);
+            if (IsGrounded() && _rigidbody.velocity.y <= 1)
+                _rigidbody.AddForce(Vector2.up * _jumpSpeed, ForceMode2D.Impulse);
+        }
+        else if (_rigidbody.velocity.y > 0)
+        {
+            _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, _rigidbody.velocity.y / 2);
         }
     }
 
--- Assets/Scripts/PalmCrownReaction.cs
+++ Assets/Scripts/PalmCrownReaction.cs
@@ -4,23 +4,23 @@ using UnityEngine;
 
 public class PalmCrownReaction : MonoBehaviour
 {
-    private EdgeCollider2D _edgeCollider;
+    [SerializeField] private Collider2D _crownCollider;
+    [SerializeField] private LayerMask _charactersMask;
     private BoxCollider2D _boxCollider;
-    [SerializeField] private LayerMask _layerMask;
 
     private void Awake()
     {
-        _edgeCollider = GetComponent<EdgeCollider2D>();
         _boxCollider = GetComponent<BoxCollider2D>();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (_boxCollider.IsTouchingLayers(_layerMask)) _edgeCollider.enabled = false;
+        if (_boxCollider.IsTouchingLayers(_charactersMask))
+            _crownCollider.enabled = false;
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        _edgeCollider.enabled = true;
+        _crownCollider.enabled = true;
     }
 }
