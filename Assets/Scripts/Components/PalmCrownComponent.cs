bc298216b629ad9c17705372fe344c91bdd31cd8
Fenneig <ninetailedkot92@gmail.com>
2021-05-22 01:28:00 +0300

    final palm realisation + coin collecting

---
+++ Assets/Scripts/Components/PalmCrownComponent.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PixelCrew.Components
+{
+    public class PalmCrownComponent : MonoBehaviour
+    {
+        [SerializeField] private Hero _hero;
+        private PlatformEffector2D _effector;
+        private float _waitTime = 0.01f;
+
+        private void Awake()
+        {
+            _effector = GetComponent<PlatformEffector2D>();
+        }
+
+        private void Update()
+        {
+            if (_hero.GetDirection().y < 0) 
+            {
+                if (_waitTime <= 0)
+                {
+                    _effector.rotationalOffset = 180f;
+                    _waitTime = 0.01f;
+                }
+                else {
+                    _waitTime -= Time.deltaTime;
+                }
+            }
+
+            if (_hero.GetDirection().y > 0) 
+            {
+                _waitTime = 0.01f;
+                _effector.rotationalOffset = 0f;
+            }
+        }
+    }
+}
+
--- Assets/Scripts/Hero.cs
+++ Assets/Scripts/Hero.cs
@@ -2,52 +2,41 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Hero : MonoBehaviour
+namespace PixelCrew
 {
-    [SerializeField] private float _speed;
-    [SerializeField] private float _jumpSpeed;
-
-    [SerializeField] private LayerCheck _groundCheck;
-
-    private Rigidbody2D _rigidbody;
-    private Vector2 _direction;
-
-    private void Awake()
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class Hero : MonoBehaviour
     {
-        _rigidbody = GetComponent<Rigidbody2D>();
-    }
 
-    public void SetDirection(Vector2 direction) 
-    {
-        _direction = direction;
-    }
+        [SerializeField] private float _speed;
+        [SerializeField] private float _jumpSpeed;
+        [SerializeField] private LayerCheck _groundCheck;
 
-
-    public void SaySomething() 
-    {
-        Debug.Log("Something");
-    }
-
-    private bool IsGrounded() 
-    {
-        return _groundCheck.IsTouchingLayer();
-    }
-
-    private void FixedUpdate()
-    {
-        _rigidbody.velocity = new Vector2(_direction.x * _speed, _rigidbody.velocity.y);
-
-        var isJumping = _direction.y > 0;
-
-        if (isJumping)
+        private Rigidbody2D _rigidbody;
+        private Vector2 _direction;
+        private void Awake()
         {
-            if (IsGrounded() && _rigidbody.velocity.y <= 0)
-                _rigidbody.AddForce(Vector2.up * _jumpSpeed, ForceMode2D.Impulse);
+            _rigidbody = GetComponent<Rigidbody2D>();
         }
-        else if (_rigidbody.velocity.y > 0)
+        public void SetDirection(Vector2 direction) => _direction = direction;
+        public Vector2 GetDirection() => _direction;
+        public void SaySomething() => Debug.Log("Something");
+        private bool IsGrounded() => _groundCheck.IsTouchingLayer();
+        private void FixedUpdate()
         {
-            _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, _rigidbody.velocity.y / 2);
+            _rigidbody.velocity = new Vector2(_direction.x * _speed, _rigidbody.velocity.y);
+
+            var isJumping = _direction.y > 0;
+
+            if (isJumping)
+            {
+                if (IsGrounded() && _rigidbody.velocity.y <= 0)
+                    _rigidbody.AddForce(Vector2.up * _jumpSpeed, ForceMode2D.Impulse);
+            }
+            else if (_rigidbody.velocity.y > 0)
+            {
+                _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, _rigidbody.velocity.y / 2);
+            }
         }
     }
-
 }
