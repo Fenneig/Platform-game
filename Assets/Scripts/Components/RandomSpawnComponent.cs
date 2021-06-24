65101a8a8946de3addc3144c81d8845a26bbf0fc
Fenneig <ninetailedkot92@gmail.com>
2021-06-25 00:44:13 +0300

    Revert "Update RandomSpawnComponent.cs"
    This reverts commit b8ba9dece3e3694bea68b6a4b377535a2a8e15cd.

--- Assets/Scripts/Components/RandomSpawnComponent.cs
+++ Assets/Scripts/Components/RandomSpawnComponent.cs
@@ -20,8 +20,8 @@ namespace PixelCrew.Components
                     float chance = UnityEngine.Random.value * 100;
                     if (chance <= component.ChanceToSpawn)
                     {
-                        Vector3 newPosition = transform.position + new Vector3(UnityEngine.Random.value - 0.5f, UnityEngine.Random.value, 0);
-                        var instance = Instantiate(component.Prefab, transform.position, Quaternion.identity);
+                        Vector3 newPosition = component.Target.position + new Vector3(UnityEngine.Random.value - 0.5f, UnityEngine.Random.value, 0);
+                        var instance = Instantiate(component.Prefab, component.Target.position, Quaternion.identity);
 
                         var rigidbody = instance.GetComponent<Rigidbody2D>();
                         if (rigidbody != null) rigidbody.AddForce(Vector2.up);
@@ -37,10 +37,12 @@ namespace PixelCrew.Components
     [Serializable]
     public class ChanceObjectToSpawn
     {
+        [SerializeField] private Transform _target;
         [SerializeField] private GameObject _prefab;
         [Range(0, 100)] [SerializeField] private float _chanceToSpawn;
         [Min(1)] [SerializeField] private int _amout = 1;
 
+        public Transform Target { get => _target; }
         public GameObject Prefab { get => _prefab; }
         public float ChanceToSpawn { get => _chanceToSpawn; }
         public int Amout { get => _amout; }
